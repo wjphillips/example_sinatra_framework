ecd19b0b69c407f07fd6ab76fbed46f1cf9650ce
Bill Phillips <wjphillips_git@outlook.com>
2015-10-19 22:24:24 -0700

    initial commit

---
+++ frank.rb
@@ -0,0 +1,9 @@
+require 'sinatra'
+require 'active_record'
+require 'bcrypt'
+Dir.glob('./{controllers,models}/*.rb').each { |file| require file }
+ENV['SINATRA_ENV'] ||= 'development'
+ActiveRecord::Base.establish_connection(
+	:adapter => 'sqlite3',
+	:database => "./db/#{ENV['SINATRA_ENV']}.sqlite"
+)
